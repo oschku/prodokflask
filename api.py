1254256336fed17a075a8b5be41b109392481e30
oschku <honkasalo.oskari@gmail.com>
2021-02-10 15:10:43 +0200

    changed yml filename

--- api.py
+++ api.py
@@ -5,12 +5,12 @@ from flask_swagger_ui import get_swaggerui_blueprint
 #create the application instance
 #app = Flask(__name__, template_folder="templates")
 
-#let connexion handles the initiation of app
+#let connexion handle the initiation of app
 app = connexion.App(__name__, specification_dir='./')
 
 
 #force app to read swagger.yml file to configure the endpoints
-app.add_api('swagger.yml')
+app.add_api('specfile.yml')
 
 SWAGGER_URL = '/api/ui'
 
